b07ec577357c98887389dcbe31dbe6f7d78b56d2
Tadek T <Ai1@interia.pl>
2024-08-17 14:41:51 +0200

    Merge two sorted arrays v3
    Merge arrays and remove duplicates

--- merge_arrays.py
+++ merge_arrays.py
@@ -2,12 +2,11 @@ def merge_arrays(arr1, arr2):
     arr1.extend(arr2)
     arr1.sort()
 
-    for i in range(len(arr1)):
-        count = arr1.count(i)
-        if count > 1:
-            arr1.pop(arr1.index(i))
-            count = 1
-    # print(arr1, end =" ")
-    return arr1
+    arr3 = []
+    for i in arr1:
+        if i not in arr3:
+            arr3.append(i)
 
-print(merge_arrays([-100, -34, -27, -27, -8, 5, 6, 12, 23, 25, 56, 56, 124, 213, 325, 325, 601], []))
+    return arr3
+
+print(merge_arrays([-100, -34, -27, -27, -8, 5, 5, 6, 12, 23, 25, 56, 56, 124, 213, 325, 325, 601], []))
